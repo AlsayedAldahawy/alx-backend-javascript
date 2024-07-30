13404dded02b8cdef514e3758621fa0c60f28fa1
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-07-30 17:07:52 +0300

    0x00-ES6_basic update

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+export default function createIteratorObject(report) {
+  const employees = [];
+
+  for (const value of Object.values(report.allEmployees)) {
+    employees.push(...value);
+  }
+
+  return employees;
+}
