680fbe7fa7159a29a1604cb1af809b6240dc471c
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-07-30 16:42:39 +0300

    0x00-ES6_basic update

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+export default function createReportObject(employeesList) {
+  const obj = {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments() { return Object.keys(this.allEmployees).length; },
+  };
+
+  return obj;
+}
