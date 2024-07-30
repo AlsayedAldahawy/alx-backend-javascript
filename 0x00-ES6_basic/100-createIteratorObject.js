a8722003536e5af66562783a1bd797d04e899c79
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-07-30 17:13:48 +0300

    Task-100

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,16 @@
 export default function createIteratorObject(report) {
   const employees = [];
-
   for (const value of Object.values(report.allEmployees)) {
     employees.push(...value);
   }
 
   return employees;
+
+  // Another method
+  // return Object.values(report.allEmployees).flat();
+
+  /**
+   * Object.values(report.allEmployees) : gets all the employees names.
+   * flat() method convert nested arrays into single flat array.
+   */
 }
