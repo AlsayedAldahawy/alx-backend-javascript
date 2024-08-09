44bf621ae9a3b3bc7b845a010ee36809041ea823
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-08-09 21:24:25 +0300

    0x01-ES6_promise update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.all([
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(filename),
   ]).then((results) => results.map(({ status, value, reason }) => ({
