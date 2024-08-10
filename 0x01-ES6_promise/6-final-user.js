92a64749afc336b3c413119d05d6cfec1a0f6ee5
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-08-10 18:16:15 +0300

    BKEND_JS_01_06

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,11 +2,19 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(filename),
-  ]).then((results) => results.map(({ status, value, reason }) => ({
-    status,
-    value: status === 'fulfilled' ? value : reason.toString(),
-  })));
+    return Promise.allSettled([
+        signUpUser(firstName, lastName),
+        uploadPhoto(filename),
+    ]).then((results) => {
+
+        const arr = [];
+        results.forEach(result => {
+            if (result.status === 'fulfilled') {
+                arr.push({'status' : result.status, 'value': result.value})
+            } else{
+                arr.push({'status' : result.status, 'value': result.reason})
+            }
+        })
+        return(arr);
+    });
 }
