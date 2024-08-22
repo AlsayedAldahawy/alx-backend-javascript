0b1a1791909e4483601289e975a8a3a2d807a1c2
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-08-22 19:51:51 +0300

    BKEND_JS_02_04

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,37 @@
+import Currency from './3-currency';
+
+class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency should be an instance of class Currency');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
