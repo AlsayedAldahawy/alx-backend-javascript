e63433ef9ce7edf36b3a2c0127b203d3a189b28d
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-08-22 20:19:13 +0300

    BKEND_JS_02_05

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') throw new TypeError('sqft must be number');
+    this._sqft = value;
+  }
+}
+
+export default Building;
