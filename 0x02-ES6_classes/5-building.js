383268ea40fc504e8d87b6782c61c73be1a50907
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-08-22 20:26:55 +0300

    BKEND_JS_02_05

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 class Building {
   constructor(sqft) {
-    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
 
@@ -18,3 +18,20 @@ class Building {
 }
 
 export default Building;
+
+/**
+ * Notes:
+     * /// new.target ///
+     * Definition: Refers to the constructor that was directly invoked with the new keyword.
+     * Usage: Useful in constructors to determine if the class is being instantiated directly or
+     * through inheritance.
+     */
+
+/**
+     * Context: this.constructor is used within instance methods to refer to the class that created
+     * the instance, while new.target is used within constructors to refer to the constructor that
+     * was called with new.
+     * Purpose: this.constructor helps in accessing class-level properties and methods from an
+     * instance,whereas new.target helps in controlling instantiation behavior, especially in
+     * inheritance scenarios.
+     */
