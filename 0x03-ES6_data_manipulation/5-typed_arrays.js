545a31fe204a0d146b926570f914a0eb40bb6589
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-08-28 02:06:03 +0300

    BKEND_JS_03_05

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,19 @@
 function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   try {
-    const int8 = new Int8Array(buffer, position, 1);
-    int8[0] = value;
+    // method 1:
+    // const int8 = new Int8Array(buffer, position, 1);
+    // // the array has only one element and the array starts at the position
+    // int8[0] = value;
+
+    // method 2:
+    const int8 = new Int8Array(buffer, 0, length);
+    int8.set([value], position); // typedArray.set(array, offset)
+    /**
+     * set method of a typed array like Int8Array expects an array or another typed array
+     * as its first argument.
+     * This is why you need to use [value] instead of value without brackets.
+    */
   } catch (error) {
     throw new RangeError('Position outside range');
   }
