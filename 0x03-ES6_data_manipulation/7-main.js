81b3b14fae02c348a060160bb6446c86995ec24a
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-08-28 02:42:12 +0300

    BKEND_JS_03_07

--- 0x03-ES6_data_manipulation/7-main.js
+++ 0x03-ES6_data_manipulation/7-main.js
@@ -3,3 +3,9 @@ import hasValuesFromArray from "./7-has_array_values";
 console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1]));
 console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [10]));
 console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1, 10]));
+
+const array = [1, 2, 'test'];
+  const set = new Set(array);
+
+
+console.log(hasValuesFromArray(set, array));
