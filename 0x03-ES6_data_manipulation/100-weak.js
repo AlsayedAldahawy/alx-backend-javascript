ebf987b6628ed0856fecf67c9941a9816f1a9d38
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-08-28 05:11:17 +0300

    BKEND_JS_03_100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,13 +1,25 @@
+/* eslint-disable no-unused-expressions */
 const weakMap = new WeakMap();
 
-let callCounter = 0;
+// first method: using Global counter
+// Global Counter: callCounter is a global variable, which means it counts calls across
+// all endpoints, not individually.
+// This could lead to incorrect behavior if multiple endpoints are being queried.
+
+// let callCounter = 0;
+
+// function queryAPI(endpoint) {
+//   callCounter += 1;
+//   if (callCounter >= 5) throw new Error('Endpoint load is high');
+//   weakMap.set(endpoint, callCounter);
+// }
 
 function queryAPI(endpoint) {
-  callCounter += 1;
-  if (callCounter >= 5) {
-    throw new Error('Endpoint load is high');
-  }
-  weakMap.set(endpoint, callCounter);
+  if (weakMap.has(endpoint) && weakMap.get(endpoint) >= 4) throw new Error('Endpoint load is high');
+
+  weakMap.has(endpoint)
+    ? weakMap.set(endpoint, weakMap.get(endpoint) + 1)
+    : weakMap.set(endpoint, 1);
 }
 
 export { queryAPI, weakMap };
