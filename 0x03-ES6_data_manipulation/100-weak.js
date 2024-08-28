19c811bd5a7ec5745fc5c3fb5a2bb316eb104b4b
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-08-28 05:14:07 +0300

    BKEND_JS_03_100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -14,12 +14,15 @@ const weakMap = new WeakMap();
 //   weakMap.set(endpoint, callCounter);
 // }
 
+// second method: using endpoint value
+// Endpoint-Specific: Uses a WeakMap to track the number of calls per endpoint,
+// ensuring each endpoint is monitored individually.
 function queryAPI(endpoint) {
-  if (weakMap.has(endpoint) && weakMap.get(endpoint) >= 4) throw new Error('Endpoint load is high');
-
   weakMap.has(endpoint)
     ? weakMap.set(endpoint, weakMap.get(endpoint) + 1)
     : weakMap.set(endpoint, 1);
+
+  if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
 }
 
 export { queryAPI, weakMap };
