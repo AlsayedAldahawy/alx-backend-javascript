780aed112d0ab99b1f3bff22e0f294f9ae2363c2
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-08-28 03:22:10 +0300

    BKEND_JS_03_08

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (startString === undefined) {
+    return '';
+  }
   return [...set].filter((string) => string.startsWith(startString) && startString !== '')
     .map((string) => string.slice(startString.length)).join('-');
 }
