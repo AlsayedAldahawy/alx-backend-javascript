9a099ddf481b98edd1d5324d27f7eb61e6954adb
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-09-01 19:26:13 +0300

    BKEND_JS_04_04

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -24,22 +24,59 @@ const printTeacher: printTeacherFunction = (firstName: string, lastName: string)
     return `${firstName[0]}.${lastName}`
 }
 
-const teacher3: Teacher = {
-    firstName: 'John',
-    fullTimeEmployee: false,
-    lastName: 'Doe',
-    location: 'London',
-    contract: false,
-};
+// task 4: interface for StudentClass
+interface classInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
 
-const director1: Directors = {
-    firstName: 'John',
-    lastName: 'Doe',
-    location: 'London',
-    fullTimeEmployee: true,
-    numberOfReports: 17,
-};
+}
 
-console.log("Director", director1);
-console.log("Teacher", teacher3);
-console.log(printTeacher(teacher3.firstName, teacher3.lastName));
+// task 4: interface for StudentClass constructor
+interface StudentConstructor {
+    (firstName: string, lastName: string): classInterface;
+}
+
+// task 4: StudentClass
+class StudentClass implements classInterface {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working"
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+
+const student = new StudentClass("Alsayed", "Aldahawy");
+console.log(student.firstName, student.lastName, student.workOnHomework(), student.displayName());
+
+
+// const teacher3: Teacher = {
+//     firstName: 'John',
+//     fullTimeEmployee: false,
+//     lastName: 'Doe',
+//     location: 'London',
+//     contract: false,
+// };
+
+// const director1: Directors = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     location: 'London',
+//     fullTimeEmployee: true,
+//     numberOfReports: 17,
+// };
+
+// console.log("Director", director1);
+// console.log("Teacher", teacher3);
+// console.log(printTeacher(teacher3.firstName, teacher3.lastName));
