8841ded2d181b2ac2a5e6089f0ed29026587c363
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-09-01 17:26:05 +0300

    BKEND_JS_04_03

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,15 +1,27 @@
+// task 1: interface for Teacher
 interface Teacher {
-    readonly firstName : string,
-    readonly lastName : string,
-    fullTimeEmployee : boolean,
-    yearsOfExperience? : number,
+    readonly firstName: string,
+    readonly lastName: string,
+    fullTimeEmployee: boolean,
+    yearsOfExperience?: number,
     location: string,
     [key: string]: any; // Index signature to allow additional properties
-    
+
 }
 
+// task 2: interface Directors
 interface Directors extends Teacher {
     numberOfReports: number;
+}
+
+// task 3: interface for function
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+// task 3: function printTeacher returns a formatted name
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string) => {
+    return `${firstName[0]}.${lastName}`
 }
 
 const teacher3: Teacher = {
@@ -18,16 +30,16 @@ const teacher3: Teacher = {
     lastName: 'Doe',
     location: 'London',
     contract: false,
-  };
-  
-  const director1: Directors = {
+};
+
+const director1: Directors = {
     firstName: 'John',
     lastName: 'Doe',
     location: 'London',
     fullTimeEmployee: true,
     numberOfReports: 17,
-  };
+};
 
-  console.log("Director", director1);
-  console.log("Teacher", teacher3);
-
+console.log("Director", director1);
+console.log("Teacher", teacher3);
+console.log(printTeacher(teacher3.firstName, teacher3.lastName));
